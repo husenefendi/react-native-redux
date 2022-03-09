bff4af1fc9fc3af536b99b14410bea3c3175a907
Husen <husen@softwareseni.com>
2022-03-09 13:52:09 +0700

    finish andd twnrc

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -6,8 +6,10 @@ import { homeViewProps } from '~type/RootStackPrams';
 
 const HomeView = ({ navigation }: homeViewProps) => {
   return (
-    <View style={tw`container justify-center`}>
-      <Text style={tw`text-sky-500 dark:text-sky-400`}>Home View</Text>
+    <View style={tw`flex justify-center p-5 h-full`}>
+      <Text style={tw`text-sky-500 dark:text-sky-400 text-center`}>
+        Home View
+      </Text>
       <Button
         title="Move to Example Page"
         onPress={() => navigation.navigate('ExamplePage')}
